7b8a506266a5873bb1653ae937806e8a2f230b74
Sivaram <katta.sivaram@suprsend.com>
2021-12-18 16:56:31 +0530

    added flush improvements

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const config = {
   api_url: "https://hub.suprsend.com",
   sdk_version: package_data.version,
   batch_size: 20,
+  flush_interval: 3000,
   service_worker_file: "serviceworker.js",
   sw_delay: 5000, //in ms,
 };
